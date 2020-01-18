70acbd73e4b9d6648125413681edef2ec2c96a50
Dev <huntinghour@gmail.com>
2020-01-18 17:26:57 +0530

    number of occurrence of yes and no in the description

---
+++ pandas_test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+y=1
+n=1
+df = pd.read_csv('INvideos.csv', sep = ',')
+print(df)
+for d in df.description:
+	if ('yes' in str(d)):
+		y = y+1;
+	elif ('no' in str(d)):
+		n = n+1
+print("number of yes in description: ",y)
+print("number of no in description: ",n)
